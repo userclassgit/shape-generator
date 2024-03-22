67c91437b8b4e0cdfcfcb75f8c02b5ddc655dc6f
Myeongjeon Lee <userclassdev@outlook.com>
2024-03-22 14:13:59 -0500

    fix font issues

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 import * as utils from './utils.js';
-import * as Shape from './Shape.js';
+import Shape from './Shape.js';
 'use strict';
 
 const shapeSelection = utils.select('.shape-selection');
@@ -7,20 +7,42 @@ const colorSelection = utils.select('.color-selection');
 const createButton = utils.select('.create-button');
 const shapeBox = utils.select('.shape-box');
 
-utils.listen('click', createButton, () => {
-  const shapeType = shapeSelection.value; const shapeColor = colorSelection.value;
+let shapes = [];
 
-  let newShape;
-  
-  if (shapeType === 'circle') { 
-    newShape = document.createElement('div'); 
-    newShape.classList.add('circle'); 
-  } else if (shapeType === 'square') { 
-    newShape = document.createElement('div'); 
-    newShape.classList.add('square'); 
+utils.listen('click', createButton, () => {
+  const shapeType = shapeSelection.value;
+  const shapeColor = colorSelection.value;
+
+  if (shapes.length < 35) { //if shapeBox isn't full, add a new shape
+    let newShapeInstance = new Shape(shapeType, shapeColor);
+    shapes.push(newShapeInstance);
+
+    let newShape = document.createElement('div');
+    newShape.classList.add(newShapeInstance.name);
+    newShape.style.backgroundColor = newShapeInstance.colour;
+
+    shapeBox.appendChild(newShape);
   }
-  
-  newShape.style.backgroundColor = shapeColor;
-  
-  shapeBox.appendChild(newShape);
 });
+
+
+
+
+
+// utils.listen('click', createButton, () => {
+//   const shapeType = shapeSelection.value; const shapeColor = colorSelection.value;
+
+//   let newShape;
+  
+//   if (shapeType === 'circle') { 
+//     newShape = document.createElement('div'); 
+//     newShape.classList.add('circle'); 
+//   } else if (shapeType === 'square') { 
+//     newShape = document.createElement('div'); 
+//     newShape.classList.add('square'); 
+//   }
+  
+//   newShape.style.backgroundColor = shapeColor;
+  
+//   shapeBox.appendChild(newShape);
+// });
