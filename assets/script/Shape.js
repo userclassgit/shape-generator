f043bba3ebcdebdb5b6bee11ea8b93b80c83ebea
Myeongjeon Lee <userclassdev@outlook.com>
2024-03-22 12:17:28 -0500

    the functionality works. have not styled the page yet.

---
+++ assets/script/Shape.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class Shape {
+  #name;
+  #colour;
+
+  constructor(name, colour) {
+    this.#name = name;
+    this.#colour = colour;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get colour() {
+    return this.#colour;
+  }
+
+  getInfo() {
+    return `${this.#colour} ${this.#name}`;
+  }
+}
+
+export default Shape;
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,26 @@
 import * as utils from './utils.js';
+import * as Shape from './Shape.js';
 'use strict';
+
+const shapeSelection = utils.select('.shape-selection');
+const colorSelection = utils.select('.color-selection');
+const createButton = utils.select('.create-button');
+const shapeBox = utils.select('.shape-box');
+
+utils.listen('click', createButton, () => {
+  const shapeType = shapeSelection.value; const shapeColor = colorSelection.value;
+
+  let newShape;
+  
+  if (shapeType === 'circle') { 
+    newShape = document.createElement('div'); 
+    newShape.classList.add('circle'); 
+  } else if (shapeType === 'square') { 
+    newShape = document.createElement('div'); 
+    newShape.classList.add('square'); 
+  }
+  
+  newShape.style.backgroundColor = shapeColor;
+  
+  shapeBox.appendChild(newShape);
+});
